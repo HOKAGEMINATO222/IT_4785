b6e2cc431fc25cc99c0f521f7f5af17b5b1f0f02
HOKAGEMINATO222 <soicodoc2501@gmail.com>
2024-10-28 20:25:23 +0700

    gmail list

---
+++ Gmail/app/src/main/java/com/example/gmail/model/Email.kt
@@ -0,0 +1,9 @@
+package com.example.gmail.model
+
+data class Email(
+    val from: String,
+    val content: String,
+    val time: String,
+    val iconBackground: Int,
+    val iconText: String
+)
